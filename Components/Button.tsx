d531e1650da1b123cffc8a73ae49331bb65a4891
Wealthometer <wealthhajoh87@gmail.com>
2025-12-23 05:26:31 +0100

    Update Button.tsx

--- Components/Button.tsx
+++ Components/Button.tsx
@@ -12,8 +12,8 @@ const Button = ({
   onPress: (event: GestureResponderEvent) => void;
 }) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={() => {}}>
       <Text style={{fontSize : 34, color : Colors.white}}>{title}</Text>
+    <TouchableOpacity
     </TouchableOpacity>
   );
 };
