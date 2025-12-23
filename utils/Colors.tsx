b91cdc78e388e8a94ae37eff1cc2d9849ce40fe2
Wealthometer <wealthhajoh87@gmail.com>
2025-12-23 05:29:57 +0100

    Update Colors.tsx

--- utils/Colors.tsx
+++ utils/Colors.tsx
@@ -1,8 +1,8 @@
 export const Colors = {
     light : '#FBFBFB',
     dark : '#0c263b',
-    btnLight : '#F6F5F2',
     btnDark : '#09122C',
+    btnLight : '#F6F5F2',
     btnRight : '#7743DB',
     black : '#171617',
     white : '#fefeff',
