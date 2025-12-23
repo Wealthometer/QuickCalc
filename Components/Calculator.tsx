db905b3a006567afcf13c27a0484498dc586da3c
Wealthometer <wealthhajoh87@gmail.com>
2025-12-23 05:29:26 +0100

    Update Calculator.tsx

--- Components/Calculator.tsx
+++ Components/Calculator.tsx
@@ -97,6 +97,91 @@ const Calculator = () => {
         </Text>
       </View>
       <View style={styles.keypad}>
+        <Button title="C" type="top" onPress={() => handleClear()} />
+        <Button title="⌫" type="top" onPress={() => handleDelete()} />
+        <Button title="%" type="top" onPress={() => handleOperatorInput("%")} />
+        <Button
+          title="∻"
+          type="right"
+          onPress={() => handleOperatorInput("/")}
+        />
+        <Button
+          title="7"
+          type="number"
+          onPress={() => handleNumberInput("7")}
+        />
+        <Button
+          title="8"
+          type="number"
+          onPress={() => handleNumberInput("8")}
+        />
+        <Button
+          title="9"
+          type="number"
+          onPress={() => handleNumberInput("9")}
+        />
+        <Button
+          title="x"
+          type="right"
+          onPress={() => handleOperatorInput("*")}
+        />
+        <Button
+          title="6"
+          type="number"
+          onPress={() => handleNumberInput("6")}
+        />
+        <Button
+          title="5"
+          type="number"
+          onPress={() => handleNumberInput("5")}
+        />
+        <Button
+          title="4"
+          type="number"
+          onPress={() => handleNumberInput("4")}
+        />
+        <Button
+          title="-"
+          type="right"
+          onPress={() => handleOperatorInput("-")}
+        />
+        <Button
+          title="3"
+          type="number"
+          onPress={() => handleNumberInput("3")}
+        />
+        <Button
+          title="2"
+          type="number"
+          onPress={() => handleNumberInput("2")}
+        />
+        <Button
+          title="1"
+          type="number"
+          onPress={() => handleNumberInput("1")}
+        />
+        <Button
+          title="+"
+          type="right"
+          onPress={() => handleOperatorInput("+")}
+        />
+        <Button
+          title="0"
+          type="number"
+          onPress={() => handleNumberInput("0")}
+        />
+        <Button
+          title="00"
+          type="number"
+          onPress={() => handleNumberInput("00")}
+        />
+        <Button
+          title="."
+          type="number"
+          onPress={() => handleNumberInput(".")}
+        />
+        <Button title="=" type="right" onPress={() => handleEqualInput()} />
+      </View>
     </View>
   )
 }
