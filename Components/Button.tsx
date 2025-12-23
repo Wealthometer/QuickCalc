3d99cc1f4bd2a40b45de5594e34949e5039c082f
Wealthometer <wealthhajoh87@gmail.com>
2025-12-23 01:07:57 +0100

    Create Button.tsx

---
+++ Components/Button.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const Button = ({title, type}: {title : string, type : 'top' | 'right' | 'number'}) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={() => {}}>
+      <Text style={{fontSize : 34, color : Colors.white}}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+    button : {
+        height : 70,
+        width : 70,
+        borderRadius: 10,
+        padding : 10,
+        alignItems : 'center',
+        justifyContent : 'center',
+        backgroundColor : Colors.btnDark
+    }
+});
